d303082dfc034072c5382f133ddc0be547e9548e
Seth Ruji <srujijunk@gmail.com>
2013-06-04 09:36:33 -0400

    Core Skeleton should be setup.
    was unsure where to put the business entities so I made a folder for
    them

--- DICK.CORE/Controllers/MaintenanceWindowController.cs
+++ DICK.CORE/Controllers/MaintenanceWindowController.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace DowntimeIntervalCalendarKeeper.Controllers
+namespace DICK.CORE.Controllers
 {
-    public class HomeController : Controller
+    public class MaintenanceWindowController : Controller
     {
+        //
+        // GET: /Maintenance/
+
         public ActionResult Index()
         {
             return View();
         }
+
     }
 }
---
+++ DICK.CORE/Repositories/ProductRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DICK.CORE.Repositories
+{
+    public class ProductRepo
+    {
+    }
+}
